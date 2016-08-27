aea88761a522aa62ecf7580041bdaf9e5e329cb1
GhuStar <qkpigstar@gmail.com>
2016-08-27 13:20:10 +0800

    1.泡沬排序法 2.調整檔案位置 3.調整pom Junit

---
+++ commons/src/main/java/commons/test/Test_Sort.java
@@ -0,0 +1,42 @@
+package commons.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import commons.Utils_Sort_Manger;
+
+public class Test_Sort {
+
+	Utils_Sort_Manger sort;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		sort = Utils_Sort_Manger.singleton;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIntArrayBubbleSort() {
+		sort.bubbleSort(sort.genRandomIntList(100,10));
+	}
+	
+	@Test
+	public void testIntListBubbleSort() {
+		sort.bubbleSort(sort.genRandomIntList(100,10));
+	}
+
+}
